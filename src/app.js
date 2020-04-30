ae10ab148ea7be168fe913ebfebdde79548e1af9
nevelson <nevelson92@gmail.com>
2020-04-30 11:25:19 -0400

    removed warnings

--- src/app.js
+++ src/app.js
@@ -14,6 +14,7 @@ app.use(helmet());
 app.use(morgan(morganOption));
 
 
+// eslint-disable-next-line no-unused-vars
 app.use(function errorHandler(error, req, res, next) {
   let response;
   if (NODE_ENV === 'production') {
--- src/server.js
+++ src/server.js
@@ -3,5 +3,6 @@ const { PORT } = require('./config');
 
 
 app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
   console.log(`Server listening at http://localhost:${PORT}`);
 });
